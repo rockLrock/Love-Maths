f6661682574ab1f357cd8338b5d1f53e37aa25ea
rockLrock <holyli2002@gmail.com>
2025-12-15 16:59:35 +0000

    Display addition question to the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });  
      }
+    runGame();
 });
 
 /**
  * the main game "loop" called when the script is loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
     
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -30,6 +37,10 @@ function checkAnswer() {}
 function calculateCorrectAnswer() {}
 function incrementScore() {}
 function incrementWrongAnswer() {}
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+}
 function displaySubtractionQuestion() {}
 function displayMultiplyQuestion() {}
