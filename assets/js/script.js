d022abcda44c2ab49dbcb94ebd8ebf63ee0342ad
rockLrock <holyli2002@gmail.com>
2025-12-16 11:25:00 +0000

    Add multiplication code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });  
      }
     runGame("addition");
+    runGame("multiply");
 });
 
 /**
@@ -23,6 +24,8 @@ function runGame(gameType) {
     
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -53,6 +56,8 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if(operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -75,4 +80,8 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 function displaySubtractionQuestion() {}
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+}
